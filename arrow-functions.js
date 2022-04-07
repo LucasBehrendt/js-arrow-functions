8c58a8ba3e086865bb47207a6f2d93261afc39e3
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-04-07 12:09:11 +0000

    initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,39 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(5, 5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // code block
+    return a + b;
+}
+let sum = addTwoNumbers(5, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(3, 3);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("hello");
+
+const saySomething2 = () => console.log("hello");
+saySomething2();
 
 // Returning Multiple Lines
+
+const multiline = () => (
+    `<p>
+        This is a multiline string!
+    <p>`
+)
+console.log(multiline());
